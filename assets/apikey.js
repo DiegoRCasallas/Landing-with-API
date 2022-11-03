2e4a76f1df6b6f07fc30c803d16fb67fcb94c919
DiegoCasallas <master.drcd@gmail.com>
2022-11-03 17:16:53 -0500

    Updates

---
+++ assets/apikey.js
@@ -0,0 +1,8 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '525d8cdd32msh1a9eb43a230b33ep17d4cajsn0dd9e8dab7ef',
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+};
+export default options;
---
+++ assets/main.js
@@ -0,0 +1,48 @@
+/*Objetivo: llamar ultimos videos de un canal de youtube En Rapiapi tenemos acceso
+ a variadas apis gratuitas y de pago*/
+
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC5HdVtarytnZi4eLogHAfoQ&part=snippet%2Cid&order=date&maxResults=8';
+
+/*referencia para la info de la api mediante el template */
+/* Es una forma de comprobar si el elemento existe. Si no es así, será nulo. */
+const content = null || document.getElementById('content');
+/* Importación de la clave API desde un archivo separado. */
+import options from "./apikey.js";
+
+/*fetch es una promesa */
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+/*Funcion que se llama a sí mismas */
+(async () => {
+    try {
+        //optenemos el contenido json de la url
+        const videos = await fetchData(API);
+        /*plantilla de tipo html */
+        let view =
+            `
+       ${videos.items.map(video => `
+       <div class="group relative">
+       <div
+         class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+         <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+       </div>
+       <div class="mt-4 flex justify-between">
+         <h3 class="text-sm text-gray-700">
+           <span aria-hidden="true" class="absolute inset-0"></span>
+           ${video.snippet.title}
+         </h3>
+       </div>
+     </div>`).slice(0, 4).join('')}
+     `;
+
+        content.innerHTML = view;
+    } catch (error) {
+        console.log(error);
+        /*reto incrustar error en el html */
+    }
+})();
